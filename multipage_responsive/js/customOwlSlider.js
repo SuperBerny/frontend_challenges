2e6a65a7d4123b438a02746369b92f861b3c5c36
Brian Patrick <batpat772@gmail.com>
2018-09-30 22:15:39 -0700

    working on getting the owl-slider to work, kind of a pain

---
+++ multipage_responsive/js/customOwlSlider.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//   $(".owl-carousel").owlCarousel({
+//     items: 1,
+//     loop: true,
+//     margin: 0,
+//     dots: false,
+//     center: true,
+//     nav: true,
+//     repsonsive: {
+//       0: {
+//         items: 1  
+//       },
+//       600: {
+//         items: 1
+//       },
+//       1000: {
+//         items: 1
+//       }
+//     }
+//   });
+// });
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({});
+});
