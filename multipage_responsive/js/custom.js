2d0e681d9b010f1c9ec34adb796ec8c31384539a
Brian Patrick <batpat772@gmail.com>
2018-10-14 23:47:50 -0700

    its possible to have 3 nav elements clicked still

--- multipage_responsive/js/custom.js
+++ multipage_responsive/js/custom.js
@@ -1,30 +1,17 @@
 // hero-bottom-nav toggle active class
-let heroBottomTab = document.querySelectorAll('.bottom-nav-tab');
+let inactiveTabs = document.querySelectorAll('.bottom-nav-tab');
 
-for (let i = 0; i < heroBottomTab.length; i++) {
-  heroBottomTab[i].addEventListener('click', function() {
+for (let i = 0; i < inactiveTabs.length; i++) {
+  inactiveTabs[i].addEventListener('click', function() {
+    let active = document.querySelector('.active');
+      if (active) {
+        active.classList.remove('active');
+      }
 
-    if (heroBottomTab[i].classList.contains('active') === true) {
-      this.classList.remove('active');
-    } else if(heroBottomTab[i].classList.contains('active') === false) {
-    this.classList.toggle('active');
-    }
-  }
-)};
-
-
-// let inactiveTabs = document.querySelectorAll('.bottom-nav-tab');
-
-// for (let i = 0; i < inactiveTabs.length; i++) {
-//   inactiveTabs[i].addEventListener('click', toggleActive);
-// }
-
-// function toggleActive() {
-//   let activeToggle = document.getElementsByClassName('bottom-nav-tab');
-//   if (activeToggle.classList.contains('active') === true) {
-//     activeToggle.classList.remove('active');
-//   } else {
-//     activeToggle.classList.add('active');
-//   }
-
-// }
+      if(inactiveTabs[i].classList.contains('active') === false) {
+        this.classList.toggle('active');
+      } else {
+        this.classList.remove('active');
+      }
+  });
+}
